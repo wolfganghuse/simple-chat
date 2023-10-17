f018a0a3435fec0698e95a43c421d37c1519ee73
wolfganghuse <wolfgang.huse@nutanix.com>
2023-10-17 12:07:46 +0200

    autodetect model

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 import argparse
+import json
 
 app = Flask(__name__)
+
+def get_default_model_name(inference_host):
+    try:
+        response = requests.get(f'http://{inference_host}:8081/models')
+        response.raise_for_status()
+        models = response.json()
+        # Returning the first model name as default
+        return models['models'][0]['modelName']
+    except Exception as e:
+        print(f"Failed to fetch the default model name from TorchServe: {e}")
+        return None
 
 @app.route('/')
 def index():
@@ -20,8 +32,16 @@ def ask():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Chat Client for TorchServe')
-    parser.add_argument('--model-name', required=True, help='The name of the model served by TorchServe')
-    parser.add_argument('--inference-host', default='localhost', help='The host where the inference server is running')
+    parser.add_argument('--model-name', help='The name of the model served by TorchServe', default=None)
+    parser.add_argument('--inference-host', help='The host where the inference server is running', default='localhost')
     args = parser.parse_args()
+    
+    if args.model_name is None:
+        args.model_name = get_default_model_name(args.inference_host)
+        if args.model_name is None:
+            print("Please provide the model name or ensure that TorchServe is running with at least one model.")
+            exit(1)
+
+    print(f"Using model: {args.model_name}")
     
     app.run(debug=True, host='0.0.0.0', port=8000)
