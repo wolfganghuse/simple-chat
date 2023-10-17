19ab53261cbb3987ce54e0c016679ae0fa03a474
wolfganghuse <wolfgang.huse@nutanix.com>
2023-10-17 09:56:17 +0200

    readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Chat client to communicate with local TorchServe instance
+
+```
+pip install -r requirements.txt
+python3 app.py --model-name your_model
+```
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/ask', methods=['POST'])
 def ask():
     user_message = request.form['user_message']
-    model_endpoint = f'http://localhost:8080/predictions/{args.model_name}'
+    model_endpoint = f'http://{args.inference_host}:8080/predictions/{args.model_name}'
     response = requests.post(model_endpoint, data=user_message)
     if response.status_code == 200:
         return jsonify({'model_response': response.text})
@@ -21,6 +21,7 @@ def ask():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Chat Client for TorchServe')
     parser.add_argument('--model-name', required=True, help='The name of the model served by TorchServe')
+    parser.add_argument('--inference-host', default='localhost', help='The host where the inference server is running')
     args = parser.parse_args()
     
     app.run(debug=True, host='0.0.0.0', port=8000)
